c3d47e9124bb54bd092cb86b10784b0bd43386f4
Ranjit Mahadik <ran.mahadik14@gmail.com>
2024-02-14 23:03:09 +0530

    - load env variables for db from .env file.

--- storage/database.go
+++ storage/database.go
@@ -2,16 +2,33 @@ package storage
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func Init() (*sql.DB, error) {
-	var err error
-	DB, err = sql.Open("postgres", "postgresql://user:user@123@localhost/flash-sale?sslmode=disable")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("[Storage]:", "Error loading .env file")
+		return nil, err
+	}
+
+	// Read environment variables
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+
+	// Construct connection string
+	connStr := fmt.Sprintf("postgresql://%s:%s@localhost/%s?sslmode=disable", dbUser, dbPassword, dbName)
+
+	// Connect to the database
+	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("[Storage]:", "couldn't connect with database")
 		return nil, err
